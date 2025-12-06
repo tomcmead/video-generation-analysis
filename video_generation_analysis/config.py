8061f95f4f37823a36cafc20ff25096ba534393c
Tom <tomcmead0@gmail.com>
2025-12-06 11:55:20 +1100

    Add keyword generation strategy using Gensim word2vec model similarity

---
+++ video_generation_analysis/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# DATABASE CONFIG
+DATABASE_PATH = "video_generation_analysis.db"
+
+# GENSIM KEYWORD MODEL
+GENSIM_MODEL_NAME = "glove-wiki-gigaword-50"
---
+++ video_generation_analysis/video_generator/keyword_gensim_strategy.py
@@ -0,0 +1,46 @@
+import logging
+
+import gensim.downloader as api
+
+from video_generation_analysis.config import GENSIM_MODEL_NAME
+from video_generation_analysis.video_generator.keyword_context import KeywordContext
+
+
+class KeywordGensimStrategy(KeywordContext):
+    """Generates new keywords using Gensim word2vec model similarity"""
+    def __init__(self):
+        self._logger: logging.Logger = logging.getLogger(__name__)
+        self._model = None
+        try:
+            self._model = api.load(GENSIM_MODEL_NAME)
+        except Exception as e:
+            self._logger.error(f"Failed to load gensim model: {e}")
+
+    def generate_keywords(
+            self, current_keywords: list[str],
+            num_new_keywords: int
+            ) -> list[str]:
+        """Generates new keywords based on current keywords using Gensim model"""
+        if self._model is None:
+            return []
+
+        new_keywords = {}
+        lower_keywords = [keyword.lower() for keyword in current_keywords]
+
+        for keyword in lower_keywords:
+            try:
+                similar = self._model.most_similar(keyword, topn=num_new_keywords)
+                for word, similarity in similar:
+                    if word not in new_keywords:
+                        new_keywords[word] = similarity
+                    else:
+                        new_keywords[word] += similarity
+
+            except KeyError:
+                self._logger.debug(f"'{keyword}' not found in model vocabulary.")
+
+        sorted_keywords = sorted(
+            new_keywords.items(), key=lambda item: item[1], reverse=True
+        )
+        keywords = [keyword[0] for keyword in sorted_keywords]
+        return keywords[:num_new_keywords]
